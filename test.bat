5f2ccec390cff70ac136dcef8ece567863f1f896
Luke <luzu8839@colorado.edu>
2025-03-02 16:40:05 -0700

    Tidies batch scripts and ensures bin dir

--- test.bat
+++ test.bat
@@ -1,8 +1,4 @@
 @echo off
-echo Declaring Model...
-python code/baseline.py
-
-echo -------------------------------------------------------------------
 echo Testing Model...
 python code/test.py
 
--- train_and_test.bat
+++ train_and_test.bat
@@ -2,6 +2,9 @@
 echo Declaring Model...
 python code/baseline.py
 
+echo Ensuring model bin folder exists.
+if not exist "bin" mkdir "bin"
+
 echo -------------------------------------------------------------------
 echo Training Model...
 python code/train.py
